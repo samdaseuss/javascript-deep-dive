893eda0de8d247d1ca3617114646c4d693ead0ac
yellpall <ohj3423@naver.com>
2023-10-18 17:43:56 +0900

    Object

--- js/식별자.js
+++ js/식별자.js
@@ -20,13 +20,18 @@ function setName() {
  * [ Object ]
  * 
  * - name : 객체의 속성
- * - ['lastName']
- *      - 셍김새 : ['속성명']
+ * - ['lastName'] : 'Oh'
+ *      - 생김새 : ['속성명']
  *      - 데이터의 식별자화 혹은 코드화
+ *      - 데이터명을 통한 속성 접근
+ * - ['changed lastName']: 'Lee'
+ *      - 브랫킷을 이용한 속성 접근
  */
 const o = {
-    name : "yunji",
-    ['lastName'] : 'Oh'
+    name : 'yunji',
+    ['lastName'] : 'Oh',
+    ['changed lastName']: 'Lee',
 }
 
 o.lastName;
+o['changed lastName'];
