c6b74aed8db18d7e47486d209608ba834686b6c1
yellpall <ohj3423@naver.com>
2023-10-18 17:35:07 +0900

    Funtion & Object

--- js/식별자.js
+++ js/식별자.js
@@ -1,9 +1,32 @@
-let name = "samdaseuss";
 /**
- * let : 키워드 혹은 명령어
- * name : 식별자
- * yunji : 데이터
+ * [ Variable ]
+ * 
+ * - let : 키워드 혹은 명령어
+ * - name : 식별자
+ * - samdaseuss : 데이터
+ */
+let name = "samdaseuss";
+
+/**
+ * [ Function ]
+ * 
+ * - setName : 식별자
  */
 function setName() {
 
 }
+
+/**
+ * [ Object ]
+ * 
+ * - name : 객체의 속성
+ * - ['lastName']
+ *      - 셍김새 : ['속성명']
+ *      - 데이터의 식별자화 혹은 코드화
+ */
+const o = {
+    name : "yunji",
+    ['lastName'] : 'Oh'
+}
+
+o.lastName;
