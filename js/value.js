0b3d44b2d2bc113f050a191d33d2a32dc60d365e
yellpall <ohj3423@naver.com>
2023-10-21 15:07:52 +0900

    value

---
+++ js/value.js
@@ -0,0 +1,19 @@
+/*
+    데이터 타입
+        * Primitive
+            * Boolean
+            * Null
+            * Undefined (*)
+            * Number : 정수, 소숫점이 있는 실수 동일 취급. 즉, 숫자
+            * String
+            * Symbol (es6)
+        * Object
+*/
+
+/* 열거 */
+const Color = {
+    Red: 1,
+    Blue: 2,
+    Green: 3
+}
+Color.Red;
