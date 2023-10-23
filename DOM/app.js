afc9381f0c0b9fbcd0793c6d0f45cb6cca4c1027
yellpall <ohj3423@naver.com>
2023-10-23 11:40:31 +0900

    createElement function

--- DOM/app.js
+++ DOM/app.js
@@ -1,4 +1,19 @@
-function createElement(c) {
-    console.log(`${c}`)
+function createElement(type, props) {
+    switch(type) {
+        case 'h1':
+            return [document.createElement('h1')]
+                .map(element => {
+                    Object
+                        .entries({ ...props, 'data-id': 'title'})
+                        .forEach(([name, value]) => element.setAttribute(name, value))
+                    return element;
+                })[0];
+        case 'div':
+            return [document.createElement('div')]
+                .map(element => {
+                    Object
+                        .entries({ ...props, 'data-id': 'layout'})
+                        .forEach(([name, value]) => element.setAttribute(name, value))
+                })[0];
+    }
 }
-console.log("hello")
