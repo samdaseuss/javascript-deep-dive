1a2afde697ea994be767a3d36421641ad548199f
yellpall <ohj3423@naver.com>
2023-10-23 11:50:23 +0900

    리팩토링

--- DOM/app.js
+++ DOM/app.js
@@ -1,19 +1,26 @@
+function createH1(props) {
+    return [document.createElement('h1')]
+        .map(element => {
+            Object
+                .entries({ ...props, 'data-id': 'title'})
+                .forEach(([name, value]) => element.setAttribute(name, value))
+            return element;
+        })[0];
+}
+
+function createDiv(props) {
+    return [document.createElement('div')]
+        .map(element => {
+            Object
+                .entries({ ...props, 'data-id': 'layout'})
+                .forEach(([name, value]) => element.setAttribute(name, value))
+            return element;
+        })[0];
+}
+
 function createElement(type, props) {
     switch(type) {
-        case 'h1':
-            return [document.createElement('h1')]
-                .map(element => {
-                    Object
-                        .entries({ ...props, 'data-id': 'title'})
-                        .forEach(([name, value]) => element.setAttribute(name, value))
-                    return element;
-                })[0];
-        case 'div':
-            return [document.createElement('div')]
-                .map(element => {
-                    Object
-                        .entries({ ...props, 'data-id': 'layout'})
-                        .forEach(([name, value]) => element.setAttribute(name, value))
-                })[0];
+        case 'h1': return createH1(props);
+        case 'div': return createDiv(props);
     }
 }
